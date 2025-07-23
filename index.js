3dc75b97d847814e1995fad2e502be50231b5ded
dayvancowboy13 <chris.e.friedrich@gmail.com>
2025-07-23 11:37:15 -0700

    Implement basic functionality outlined in project

--- index.js
+++ index.js
@@ -2,8 +2,48 @@ const http = require('node:http');
 const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'plain/text' });
-    res.end();
+
+    console.log('Server started...');
+
+    res.setHeader('Content-Type', 'text/html')
+    let path = './';
+
+    console.log(req.url, req.method);
+
+    switch (req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/contact':
+            path += 'contact-me.html';
+            res.statusCode = 200;
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+
+    fs.readFile(path, 'utf-8', (err, data) => {
+        console.log("reading file to send...")
+        if (err) {
+            console.log('error found')
+            console.log(err);
+            res.end();
+        } else {
+            console.log('Sending html page...');
+            res.end(data);
+        }
+    })
+
 });
 
-server.listen(3000);
+server.listen(8080, 'localhost', () => {
+    console.log('Listening for requests on post 8080...');
+});
